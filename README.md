b6583b7671b7e0811670dc27e06caa757526f164
hebertdev82 <hebertdev82@gmail.com>
2023-06-17 18:04:16 -0300

    project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Minas Tem Tudo de Bom
+Projeto do Painel Administrativo da Minas Tem Tudo de Bom.
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Minas Tem Tudo de Bom (ADMIN)</h1>
+    </div>
+  );
+}
+
+export default App;
